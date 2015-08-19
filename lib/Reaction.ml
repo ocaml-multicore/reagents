4d003ea2f49dc0278b6911090820909d587b086f
KC Sivaramakrishnan <sk826@cl.cam.ac.uk>
2015-08-19 22:43:25 +0100

    Removing result.

--- lib/Reaction.ml
+++ lib/Reaction.ml
@@ -9,7 +9,6 @@ module type S = sig
   val has_offer  : t -> 'a offer -> bool
   val union : t -> t -> t
   val with_post_commit : t -> (unit -> unit) -> t
-  (* val can_cas_immediate : t -> ('a,'b) Reagent.t -> 'b Offer.t -> bool *)
 end
 
 module Make (Sched: Scheduler.S) : S with type 'a offer = 'a Offer.Make(Sched).t = struct
--- lib/Reaction.mli
+++ lib/Reaction.mli
@@ -2,14 +2,13 @@ module type S = sig
   type t
   type 'a offer
   val empty : t
-  val with_CAS : t -> PostCommitCAS.t -> t
+  val with_CAS   : t -> PostCommitCAS.t -> t
   val with_offer : t -> 'a offer -> t
   val try_commit : t -> bool
   val cas_count  : t -> int
   val has_offer  : t -> 'a offer -> bool
-  val union : t -> t -> t
+  val union      : t -> t -> t
   val with_post_commit : t -> (unit -> unit) -> t
-  (* val can_cas_immediate : t -> ('a,'b) Reagent.t -> 'b Offer.t -> bool *)
 end
 
 module Make (Sched: Scheduler.S) : S with type 'a offer = 'a Offer.Make(Sched).t
