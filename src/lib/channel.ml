865fb9cb8120ed0ab94c99e902c89919ebeb268e
Nicolas ASSOUAD <nicolas@assouad.com>
2017-07-12 16:44:48 +0100

    channel using Bag

--- src/lib/channel.ml
+++ src/lib/channel.ml
@@ -29,6 +29,7 @@ module Make (Sched : Scheduler.S) : S with
   module Reagent = Reagent.Make(Sched)
   module Reaction = Reaction.Make(Sched)
   module Offer = Offer.Make(Sched)
+  module Bag = Lockfree.Bag_Custom(struct let nb_domains = 2;; end);;
 
   open Reagent
 
@@ -66,17 +67,17 @@ module Make (Sched : Scheduler.S) : S with
 
   type ('a,'b) endpoint =
     { name : string;
-      outgoing: ('a,'b) message Lockfree.MSQueue.t;
-      incoming: ('b,'a) message Lockfree.MSQueue.t }
+      outgoing: ('a,'b) message Bag.t;
+      incoming: ('b,'a) message Bag.t }
 
   let mk_chan ?name () =
-    let name = 
+    let name =
       match name with
       | None -> ""
       | Some n -> n
     in
-    let l1 = Lockfree.MSQueue.create () in
-    let l2 = Lockfree.MSQueue.create () in
+    let l1 = Bag.create () in
+    let l2 = Bag.create () in
     {name = "+" ^ name; incoming = l1; outgoing = l2},
     {name = "-" ^ name; incoming = l2; outgoing = l1}
 
@@ -86,10 +87,15 @@ module Make (Sched : Scheduler.S) : S with
     let try_react ep k a rx offer =
       let {name; outgoing; incoming} = ep in
       (* Search for matching offers *)
-      let rec try_from cursor retry =
-        match Lockfree.MSQueue.next cursor with
-        | None -> if retry then Retry else Block
-        | Some (Message (sender_offer,exchange), cursor) ->
+      let rec try_from elem retry save =
+        let rec repush_offer l out =
+          match l with
+          |h::t -> Bag.push incoming h; repush_offer t out
+          |[] -> out
+        in
+        match elem with
+        | None -> if retry then repush_offer save Retry else repush_offer save Block
+        | Some (Message (sender_offer,exchange) as elem_saved) ->
             let same_offer o = function
             | None -> false
             | Some o' -> Offer.equal o o'
@@ -97,29 +103,37 @@ module Make (Sched : Scheduler.S) : S with
             ( if (not (Offer.is_active sender_offer))
                 || Reaction.has_offer rx sender_offer
                 || same_offer sender_offer offer then
-(*                   let _ = Printf.printf "me!!\n" in *)
-                  try_from cursor retry
+(*                   let _ = Printf.printf "me!!\n" in*)
+                  try_from (Bag.pop incoming) retry (elem_saved::save)
               else (* Found matching offer *)
 (*                 let _ = Printf.printf "found matching offer!\n" in *)
                 let new_rx = Reaction.with_offer rx sender_offer in
                 let merged = exchange.compose k in
                 match merged.try_react a new_rx offer with
-                | Retry -> try_from cursor true
-                | Block | BlockAndRetry -> try_from cursor retry
-                | v -> v )
+                | Retry -> try_from (Bag.pop incoming) true (elem_saved::save)
+                | Block | BlockAndRetry -> try_from (Bag.pop incoming) retry (elem_saved::save)
+                | v ->
+(*                  print_endline "AQUI !!!!";*)
+                  repush_offer save v)
       in
       ( begin
           match offer with
           | Some offer (* when (not k.may_sync) *) ->
 (*               Printf.printf "[%d,%s] pushing offer %d\n"  *)
 (*                 (Sched.get_tid ()) name @@ Offer.get_id offer; *)
-              Lockfree.MSQueue.push outgoing (mk_message a rx k offer)
+              Bag.push outgoing (mk_message a rx k offer)
           | _ -> ()
         end;
-(*         Printf.printf "[%d,%s] checking..\n" (Sched.get_tid()) name; *)
-        Lockfree.MSQueue.clean_until incoming message_is_active;
-        if Lockfree.MSQueue.is_empty incoming then Block
-        else try_from (Lockfree.MSQueue.snapshot incoming) false )
+(*        print_endline (Printf.sprintf "[%d,%s] checking..\n" (Sched.get_tid()) name);*)
+        let rec clean_until b =
+          match Bag.pop b with
+          |Some(v) as out ->
+            if message_is_active v then
+              try_from out false []
+            else
+              clean_until b
+          |None -> Block
+        in clean_until incoming)
     in
     fun ep k ->
       { always_commits = false;
