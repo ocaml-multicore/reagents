736dc8ef5a259bca013a7d44d70a8cfb69ebed64
Vesa Karvonen <vesa.a.j.k@gmail.com>
2023-02-06 19:50:09 +0200

    Add `Domain.cpu_relax ()` to the wait loop

--- examples/lock.ml
+++ examples/lock.ml
@@ -2,7 +2,13 @@ type 'a holder = 'a option ref
 type 'a t = 'a holder Atomic.t
 
 let make initial = Atomic.make (ref (Some initial))
-let rec wait prev = match !prev with None -> wait prev | Some value -> value
+
+let rec wait prev =
+  match !prev with
+  | None ->
+      Domain.cpu_relax ();
+      wait prev
+  | Some value -> value
 
 let acquire m =
   let next = ref None in
